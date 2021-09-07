c8f2e83bdb498e6d27e23e9f317503e17012260e
zhushengyu <757521389@qq.com>
2021-09-07 16:12:34 +0800

    应用上下文

--- src/test/java/com/zsy/springframework/test/ApiTest.java
+++ src/test/java/com/zsy/springframework/test/ApiTest.java
@@ -3,11 +3,15 @@ package com.zsy.springframework.test;
 import com.zsy.springframework.beans.PropertyValue;
 import com.zsy.springframework.beans.PropertyValues;
 import com.zsy.springframework.beans.factory.config.BeanDefinition;
+import com.zsy.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import com.zsy.springframework.beans.factory.config.BeanPostProcessor;
 import com.zsy.springframework.beans.factory.config.BeanReference;
 import com.zsy.springframework.beans.factory.support.DefaultListableBeanFactory;
 import com.zsy.springframework.beans.factory.support.reader.XmlBeanDefinitionReader;
 import com.zsy.springframework.test.bean.UserDao;
 import com.zsy.springframework.test.bean.UserService;
+import com.zsy.springframework.test.common.MyBeanFactoryPostProcessor;
+import com.zsy.springframework.test.common.MyBeanPostProcessor;
 import org.junit.Test;
 
 public class ApiTest {
@@ -25,4 +29,23 @@ public class ApiTest {
         userService.queryUserInfo();
     }
 
+    @Test
+    public void testBeanFactoryPostProcessorAndBeanPostProcessor() {
+
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        beanDefinitionReader.loadBeanDefinitions("classpath:spring-02.xml");
+
+        BeanFactoryPostProcessor beanFactoryPostProcessor =
+                new MyBeanFactoryPostProcessor();
+        beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
+
+        BeanPostProcessor beanPostProcessor =
+                new MyBeanPostProcessor();
+        beanFactory.addBeanPostProcessor(beanPostProcessor);
+
+
+        UserService userService = beanFactory.getBean("userService", UserService.class);
+        userService.queryUserInfo();
+    }
 }
--- src/test/java/com/zsy/springframework/test/bean/UserService.java
+++ src/test/java/com/zsy/springframework/test/bean/UserService.java
@@ -3,10 +3,14 @@ package com.zsy.springframework.test.bean;
 public class UserService {
 
     private String uId;
+    private String company;
+    private String location;
     private UserDao userDao;
 
     public void queryUserInfo() {
-        System.out.println("查询用户信息：" + userDao.queryUserName(uId));
+
+        // System.out.println("查询用户信息：" + userDao.queryUserName(uId));
+        System.out.println(userDao.queryUserName(uId) + "," + company + "," + location);
     }
 
     public String getuId() {
@@ -23,5 +27,21 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 }
